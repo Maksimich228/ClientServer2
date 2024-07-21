ac45062600a49ab4f54598ac0a46b8fb9c531b0f
Maksimich228 <147976922+Maksimich228@users.noreply.github.com>
2024-07-21 15:38:13 +0300

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,24 +1,77 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <boost/asio.hpp>
+#include <mysql/mysql.h>
 
-void handle_client(boost::asio::ip::tcp::socket& socket) {
-    try {
-        while (true) {
-            char data[512];
-            boost::system::error_code error;
-            size_t length = socket.read_some(boost::asio::buffer(data), error);
-            if (error == boost::asio::error::eof)
-                break; 
-            else if (error)
-                throw boost::system::system_error(error); 
-
-            std::cout << "Received: " << data << std::endl;
-
-            
-        }
+void save_message_to_db(const std::string& message) {
+    MYSQL* conn = mysql_init(NULL);
+    if (conn && mysql_real_connect(conn, "localhost", "user", "password", "chat_db", 0, NULL, 0)) {
+        std::string query = "INSERT INTO messages (content) VALUES ('" + message + "')";
+        mysql_query(conn, query.c_str());
+        mysql_close(conn);
     }
-    catch (std::exception& e) {
+}
+
+std::vector<std::string> get_users_from_db() {
+    std::vector<std::string> users;
+    MYSQL* conn = mysql_init(NULL);
+    if (conn && mysql_real_connect(conn, "localhost", "user", "password", "chat_db", 0, NULL, 0)) {
+        if (mysql_query(conn, "SELECT username FROM users") == 0) {
+            MYSQL_RES* res = mysql_store_result(conn);
+            MYSQL_ROW row;
+            while ((row = mysql_fetch_row(res)) != NULL) {
+                users.push_back(row[0]);
+            }
+            mysql_free_result(res);
+        }
+        mysql_close(conn);
+    }
+    return users;
+}
+
+std::vector<std::string> get_messages_from_db() {
+    std::vector<std::string> messages;
+    MYSQL* conn = mysql_init(NULL);
+    if (conn && mysql_real_connect(conn, "localhost", "user", "password", "chat_db", 0, NULL, 0)) {
+        if (mysql_query(conn, "SELECT content FROM messages") == 0) {
+            MYSQL_RES* res = mysql_store_result(conn);
+            MYSQL_ROW row;
+            while ((row = mysql_fetch_row(res)) != NULL) {
+                messages.push_back(row[0]);
+            }
+            mysql_free_result(res);
+        }
+        mysql_close(conn);
+    }
+    return messages;
+}
+
+void handle_client(boost::asio::ip::tcp::socket socket) {
+    try {
+        char data[512];
+        boost::system::error_code error;
+        size_t length = socket.read_some(boost::asio::buffer(data), error);
+        if (error) return;
+
+        std::string message(data, length);
+        save_message_to_db(message);
+
+        std::vector<std::string> users = get_users_from_db();
+        std::vector<std::string> messages = get_messages_from_db();
+
+        std::string response = "Users:\n";
+        for (const auto& user : users) {
+            response += user + "\n";
+        }
+
+        response += "Messages:\n";
+        for (const auto& msg : messages) {
+            response += msg + "\n";
+        }
+
+        boost::asio::write(socket, boost::asio::buffer(response));
+    } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
 }
@@ -33,8 +86,7 @@ int main() {
             acceptor.accept(socket);
             std::thread(handle_client, std::move(socket)).detach();
         }
-    }
-    catch (std::exception& e) {
+    } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
 
